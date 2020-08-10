971929dbdd12adff9a222f36207c5e9462bf04e6
Ryuhei Shida <ryushidadev@gmail.com>
2020-08-10 00:22:20 -0700

    Implement filter by date for daily plan

--- src/sql.rs
+++ src/sql.rs
@@ -208,9 +208,21 @@ pub fn filter_by_date(conn: &Connection, date: &str) -> Result<Vec<Task>> {
 
 pub fn filter_by_date_plan(conn: &Connection, date: &str) -> Result<Vec<Task>> {
     let query = format!(
-        "SELECT id, name, project, start, estimate,
-                         repeat, next, notes, status FROM tasks
-                         WHERE next = '{}' ORDER BY start",
+        "SELECT t.id, t.name, t.project, t.start, t.estimate,
+        t.repeat, t.next, ifnull(n.notetext, ''), t.status
+        FROM tasks as t
+        LEFT OUTER JOIN (
+            SELECT *
+            FROM (
+            SELECT id, MIN(start) as start, notetext
+            FROM note
+            GROUP BY id)
+            WHERE date(start) >= date('now', '-1 day')
+            ORDER BY id
+        ) as n
+        on t.id = n.id
+        WHERE next = '{}'
+        ORDER BY t.start",
         date
     );
     let task_vector = query_to_vec_task(conn, &query)?;
